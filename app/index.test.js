41036b340ddcb45a66e647d10aaa7a2e838896af
Juan Macías <info@juanmacias.es>
2023-04-30 14:50:51 +0200

    Add index.test.js with sample test

---
+++ app/index.test.js
@@ -0,0 +1,7 @@
+describe('Given the cmd "npm run test"', () => {
+    describe('When the test is run', () => {
+        test('Then it should pass', () => {
+            expect(true).toBe(true)
+        })
+    })
+})
